b5d536130a7b616ca9b36c3c486d59053fc62113
Kawser Nasim <kawsernasim@Kawsers-MacBook-Pro.local>
2018-07-26 05:46:24 +0600

    server running and rendering teacher profile. deleted all mongo dependcies and started fresh

--- server/server.py
+++ server/server.py
@@ -3,16 +3,11 @@ import logging
 
 # [START imports]
 from flask import Flask, render_template, request, url_for
-#from flask_pymongo import PyMongo
-from pymongo import MongoClient
-
 
 # [END imports]
 
 # [START create_app]
 app = Flask(__name__)
-client = pymongo.MongoClient("mongodb+srv://kawser:<Nasim785%21>cluster1-unqcl.gcp.mongodb.net/test?retryWrites=true")
-db = client.test
 # [END create_app]
 
 
@@ -23,25 +18,6 @@ def form():
 # [END form]
 
 
-# [START submitted]
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-
-    # [END submitted]
-    # [START render_template]
-    return render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
-    # [END render_template]
-
-
 
 @app.errorhandler(500)
 def server_error(e):
@@ -49,14 +25,6 @@ def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 # [END app]
-
-
-
-@app.route("/db")
-def add():
-    online_users = mongo.db.users.find({"online": True})
-    return 'added user'
-
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=8080)
